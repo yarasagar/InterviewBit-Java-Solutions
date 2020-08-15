8eda591c0b7f63aa55a944637e30b64fcf019153
YARA SAGAR <yarasagar01@gmail.com>
2020-08-15 19:07:39 +0530

    Update AllFactors.java

--- Math/Examples/AllFactors.java
+++ Math/Examples/AllFactors.java
@@ -1,10 +1,17 @@
 public class Solution {
     public ArrayList<Integer> allFactors(int A) {
         ArrayList<Integer> B = new ArrayList<>();
-        for(int i=1; i<=A; i++){
-            if(A%i == 0)
-                B.add(i);
+        for(int i=1; i<=Math.sqrt(A); i++){
+            if(A%i == 0){
+                if(A/i == i)
+                    B.add(i);
+                else{
+                    B.add(i);
+                    B.add(A/i);
+                }
+            }
         }
+        Collections.sort(B);
         return B;
     }
 }

5e0cc60f83f6c817fa452047ab022fe7224ba59a
YARA SAGAR <yarasagar01@gmail.com>
2020-08-16 00:20:04 +0530

    Create IsRectangel.java

---
+++ Math/Add.Practice/IsRectangel.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solve(int A, int B, int C, int D) {
+        if(A == B && C == D)
+            return 1;
+        else if(A == C && B == D)
+            return 1;
+        else if(A == D && B == C)
+            return 1;
+        else
+            return 0;
+    }
+}

64beb762675460445f023827f0c247c5dd808c3b
YARA SAGAR <yarasagar01@gmail.com>
2020-08-25 23:16:29 +0530

    Create 1.TrailingZeros.java

---
+++ BitManipulation/Add Practice/1.TrailingZeros.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int solve(int A) {
+        for(int i=0; i<32; i++){
+            if((A&(1<<i)) != 0){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
